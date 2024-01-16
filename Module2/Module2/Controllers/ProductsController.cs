52064a3d10fe7d4b455192614c8489a5e1c4a758
TRUPTI-PTS <156814389+TRUPTI-PTS@users.noreply.github.com>
2024-01-16 19:30:20 +0530

    Add files via upload
    initial commit

---
+++ Module2/Module2/Controllers/ProductsController.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Module2.Models;
+
+namespace Module2.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ProductsController : Controller
+    {
+        static List<Product> _products = new List<Product>()
+        {
+          new Product() { ProductId = 0, ProductName = "Laptop", ProductPrice = "200" },
+          new Product() { ProductId = 1, ProductName = "Phone", ProductPrice = "300" },
+        };
+
+        [HttpGet]
+        public IEnumerable<Product> Get()
+        {
+            return _products;
+        }
+
+        public void Post([FromBody] Product product)
+        {
+            _products.Add(product);
+        }
+    }
+}
---
+++ Module2/Module2/Models/Product.cs
@@ -0,0 +1,11 @@
+﻿namespace Module2.Models
+{
+    public class Product
+    {
+        public int ProductId { get; set; }
+        public string ProductName { get; set; }
+        public string ProductPrice { get; set; }
+
+
+    }
+}
